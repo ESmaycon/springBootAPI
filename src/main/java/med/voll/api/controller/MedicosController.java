f33c84e69c78e70dd809ac14b85a43fa50b1d14d
ESmaycon <esmaycon.moraes@gmail.com>
2023-11-20 17:16:04 -0400

    Enviando dados para o banco de dados, utilizando a dependencia Flyway para fazer a migração

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,15 +1,22 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.medic.DadosCadastroMedico;
+import med.voll.api.medic.Medico;
+import med.voll.api.medic.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicosController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
+    @Transactional
     public void register(@RequestBody DadosCadastroMedico dados){
 
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
